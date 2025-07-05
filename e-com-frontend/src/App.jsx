22279f36bee9a4c91d87ad6d5734bf76ccb893f2
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-07-05 23:58:20 +0530

    created navbar component

--- e-com-frontend/src/App.jsx
+++ e-com-frontend/src/App.jsx
@@ -10,13 +10,16 @@ import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 
 function App() {
  
 
   return (
     <>
+    
       <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+        <Navbar />
         <Routes>
         <Route path='/' element={<Home />} />
          <Route path='/collection' element={<Collection />} />
