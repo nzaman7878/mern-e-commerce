27f8ffede9be670ee61f1efd9ae981eeb9e545df
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-07-08 21:25:29 +0530

    solved issue

--- e-com-frontend/src/components/LatestCollection.jsx
+++ e-com-frontend/src/components/LatestCollection.jsx
@@ -1,22 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
 
 const LatestCollection = () => {
-  const { products } = useContext(ShopContext);
-
+    const {products} = useContext(ShopContext);
+    
   return (
-    <div className='my-10 px-4 sm:px-8'>
-      <Title text1={'LATEST'} text2={'COLLECTIONS'} />
-      <p className='mt-4 max-w-[800px] mx-auto text-xs sm:text-base text-gray-600 text-center'>
-        Discover our latest collection of products that are designed to elevate your style and
-        comfort. From trendy apparel to must-have accessories, explore the freshest additions
-        to our store that cater to every taste and occasion.
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'></div>
+      <Title  text1={'LATEST'} text2={'COLLECTIONS'}/>
+      <p className='w-3/4 m-auto text-xs sm:text-base text-gray-600'>
+        Discover our latest collection of products that are designed to elevate your style and 
+        comfort. From trendy apparel to must-have accessories, explore the freshest additions 
+        to our store that cater to every taste and occasion. 
       </p>
-
-      
     </div>
-  );
-};
+  )
+}
 
-export default LatestCollection;
+export default LatestCollection
