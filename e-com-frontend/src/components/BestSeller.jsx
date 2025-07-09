99534c391e9db67e1c0900383b6d8e1547660b54
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-07-09 21:45:49 +0530

    added best seller component

---
+++ e-com-frontend/src/components/BestSeller.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const BestSeller = () => {
+  const { products } = useContext(ShopContext);
+  const [bestSeller, setBestSeller] = useState([]);
+
+  useEffect(() => {
+    const bestProduct = products.filter((item) => item.bestseller);
+    setBestSeller(bestProduct.slice(0, 5));
+  }, [products]);
+
+  return (
+    <div className='my-10 px-4 sm:px-8'>
+      <div className='text-center text-3xl py-8'>
+        <Title text1={'BEST'} text2={'SELLERS'} />
+        <p className='mt-4 max-w-[800px] mx-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Explore our most popular products loved by our customers. Hand-picked bestsellers that combine quality, comfort, and trending designs—all in one place.
+        </p>
+      </div>
+
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {bestSeller.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BestSeller;
