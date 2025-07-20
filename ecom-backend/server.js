6c15d68051ea8af332ff944796ec0a8e867c71a9
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-07-20 21:48:45 +0530

    feat: setup basic Express server with middleware configuration

---
+++ ecom-backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+// App config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// API endpoints
+app.get('/', (req, res) => {
+    res.send('API Working');
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
