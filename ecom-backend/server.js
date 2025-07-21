821310fad0001d93ba0b3cff99f63e3ca1af68ec
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-07-22 01:25:11 +0530

    fix: do not append database name to MongoDB URI in mongoose.connect

--- ecom-backend/server.js
+++ ecom-backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongoDB.js';
 import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
 
 // App config
 const app = express();
@@ -14,7 +15,12 @@ connectCloudinary();
 app.use(express.json());
 app.use(cors());
 
+
+
 // API endpoints
+
+app.use('/api/user', userRouter);
+
 app.get('/', (req, res) => {
     res.send('API Working');
 });
