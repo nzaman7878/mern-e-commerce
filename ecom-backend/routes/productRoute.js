3c66163ce6088bf4abd77c253e2d8ac75398ab9a
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-07-24 01:58:37 +0530

    feat: implement listProducts controller to fetch all products from DB

--- ecom-backend/routes/productRoute.js
+++ ecom-backend/routes/productRoute.js
@@ -20,7 +20,7 @@ productRouter.post(
 
 productRouter.post('/remove', removeProduct);
 productRouter.post('/single', singleProduct);
-productRouter.post('/list', listProducts);
+productRouter.get('/list', listProducts);
 
 
 export default productRouter;
