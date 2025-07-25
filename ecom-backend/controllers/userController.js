7a084ac9829af135b3b2f067b1b0cdd59a162a5a
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-07-25 20:17:40 +0530

    feat: add initial adminAuth middleware for JWT-based admin route protection
    - Extract token from request headers
    - Verify token using JWT and secret
    - Check token validity before allowing access to protected admin routes
    - Return error response on failed verification or missing token
    "

--- ecom-backend/controllers/userController.js
+++ ecom-backend/controllers/userController.js
@@ -121,19 +121,17 @@ const adminLogin = async (req, res) => {
     const { email, password } = req.body;
 
    
-    if (
-      email === process.env.ADMIN_EMAIL &&
+    if (email === process.env.ADMIN_EMAIL &&
       password === process.env.ADMIN_PASSWORD
     ) {
       
       const token = jwt.sign(
-        { email },                   
-        process.env.JWT_SECRET,
-        { expiresIn: '24h' }         
+        email+password,process.env.JWT_SECRET,
+               
       );
-
-      
       res.json({ success: true, token });
+      
+    
     } else {
       
       res.json({ success: false, message: 'Invalid credentials' });
