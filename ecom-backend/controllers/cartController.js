1893ff8540b24a47a1f43d018d02747ba773f6bf
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-08-03 23:59:53 +0530

    feat : updateCart function implemented

--- ecom-backend/controllers/cartController.js
+++ ecom-backend/controllers/cartController.js
@@ -46,6 +46,28 @@ const addToCart = async (req, res) => {
 // update user cart 
 
 const updateCart = async (req, res) =>{
+
+    try {
+        
+        const {userId, itemId, size, quantity} = req.body
+
+        const userData = await userModel.findById(userId)
+        let cartData = await userData.cartData;
+
+        cartData[itemId][size] = quantity
+
+        await userModel.findByIdAndUpdate(userId, {cartData})
+        res.json({success: true,
+            message:"cart Updated"
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
     
 }
 
