f643407830da8d1e6a3da87e808820d0eceb8db6
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-07-18 00:07:08 +0530

    enhanced the security

--- ecom-backend/server.js
+++ ecom-backend/server.js
@@ -13,6 +13,12 @@ import reviewRouter from './routes/reviewRoute.js';
 import discountRouter from './routes/discountRoute.js';
 import couponRouter from './routes/couponRoute.js';
 
+// Security Middlewares
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import mongoSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
+
 // App config
 const app = express();
 const port = process.env.PORT || 4000;
@@ -22,6 +28,23 @@ connectCloudinary();
 // Middlewares
 app.use(express.json());
 app.use(cors());
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Rate limiting (limit each IP to 500 requests per 15 mins)
+const limiter = rateLimit({
+    max: 500,
+    windowMs: 15 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in 15 minutes!'
+});
+app.use('/api', limiter);
 
 
 
